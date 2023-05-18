cf917c21db37c1e510efaf1255a209024caaa697
Calvin Roberts <you@example.com>
2023-05-18 15:13:44 -0600

    updated README

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -1,25 +1,27 @@
 // Assignment code here
-var passwordBox = document.getElementById("password");
-var length = 12;
+// var passwordBox = document.getElementById("password");
+// var length = 12;
 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var symbol = "@#$%^&*(){}<>/?";
+// var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+// var number = "0123456789";
+// var symbol = "@#$%^&*(){}<>/?";
 
-var allChars = upperCase + lowerCase + number + symbol;
+// var allChars = upperCase + lowerCase + number + symbol;
 
-function generatePassword(){
-  let password = "";
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += number[Math.floor(Math.random() * number.length)];
-  password += symbol[Math.floor(Math.random() * symbol.length)];
+// function generatePassword(){
+//   let password = "";
+//   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//   password += number[Math.floor(Math.random() * number.length)];
+//   password += symbol[Math.floor(Math.random() * symbol.length)];
 
-  while (length > password.length){
-    password += allChars[Math.floor(Math.random() * allChars.length)];
-  }
-}
+//   while (length > password.length){
+//     password += allChars[Math.floor(Math.random() * allChars.length)];
+//   }
+// }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
